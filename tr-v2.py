2a0d606243a83f2277adee3e0b1197e39b26960e
Kamil Rocki <kmrocki@MacBook-Pro-4.local>
2020-01-20 17:38:05 -0800

    grad

--- tr-v2.py
+++ tr-v2.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 # author: krocki
 import numpy as np
-import pprint
 
-HN = 2
-M = 3
+HN = 8
+M = 10
 S = 5
 
 datatype = np.float64
@@ -17,12 +16,12 @@ def softmax(ys):
   return ps
 
 def forward(xs, model):
-  hs = np.dot(model['Wxh'].T, xs)
-  ys = np.dot(model['Why'].T, hs)
+  zs = np.dot(model['WxVQK'].T, xs)
+  ys = np.dot(model['Wzy'].T, zs)
   ps = softmax(ys)
   states = {}
   states['xs'] = xs
-  states['hs'] = hs
+  states['zs'] = zs
   states['ps'] = ps
   return states
 
@@ -33,9 +32,9 @@ def lossfun(states, ts):
 def backward(states, model, ts):
   grads = {}
   dy = states['ps'] - ts
-  grads['Why'] = np.dot(states['hs'], dy.T)
-  grads['h'] = np.dot(model['Why'], dy)
-  grads['Wxh'] = np.dot(states['xs'], grads['h'].T)
+  grads['Wzy'] = np.dot(states['zs'], dy.T)
+  grads['z'] = np.dot(model['Wzy'], dy)
+  grads['WxVQK'] = np.dot(states['xs'], grads['z'].T)
 
   return grads
 
@@ -63,6 +62,26 @@ def samplegrad(xs, model, ts):
 
   return grads
 
+def checkgrads(xs, encoder, ts, analytical):
+  numerical = samplegrad(xs, encoder, ts)
+  errors = {}
+
+  for i,k in enumerate(numerical):
+    n = numerical[k]
+    a = analytical[k]
+    e = np.zeros_like(a)
+    nmin, nmax = None, None
+    for j,m in enumerate(n):
+      e.flat[m] = np.fabs(n[m] - a.flat[m])
+      if nmin: nmin = min(nmin, n[m])
+      else: nmin = n[m]
+      if nmax: nmax = max(nmax, n[m])
+      else: nmax = n[m]
+    errors[k] = e
+    print('{}: samples {}\n\trange n [{}, {}] \
+    \n\trange a [{}, {}]\n\tmax err = {}'.format(k, len(n), \
+    nmin, nmax, np.min(a), np.max(a), np.max(errors[k])))
+
 if __name__ == "__main__":
 
   encoder = {}
@@ -75,50 +94,12 @@ if __name__ == "__main__":
     ts[m,s] = 1
 
   # V Q K
-  #WxVQK = np.random.randn(M, 3 * HN).astype(datatype) * 0.01
-
-  Wxh = np.random.randn(M, HN).astype(datatype) * 0.01
-  Why = np.random.randn(HN, M).astype(datatype) * 0.01
-  encoder['Wxh']=Wxh
-  encoder['Why']=Why
+  WxVQK = np.random.randn(M, 3*HN).astype(datatype) * 0.01
+  Wzy = np.random.randn(HN*3, M).astype(datatype) * 0.01
+  encoder['WxVQK']=WxVQK
+  encoder['Wzy']=Wzy
 
   states = forward(xs, encoder)
   loss = lossfun(states, ts)
   analytical = backward(states, encoder, ts)
-  numerical = samplegrad(xs, encoder, ts)
-
-  #pprint.pprint(numerical)
-  nWhy = numerical['Why']
-  nWhx = numerical['Wxh']
-  dWhy = analytical['Why']
-  dWhx = analytical['Wxh']
-
-  err = {}
-  num = {}
-
-  err['Why'] = np.zeros_like(encoder['Why'])
-  err['Wxh'] = np.zeros_like(encoder['Wxh'])
-  num['Why'] = np.zeros_like(encoder['Why'])
-  num['Wxh'] = np.zeros_like(encoder['Wxh'])
-
-  for p in numerical['Why']:
-    err['Why'].flat[p]=np.fabs(numerical['Why'][p] - analytical['Why'].flat[p])
-    num['Why'].flat[p]=numerical['Why'][p]
-    #print(p - analytical.flatten())
-
-  print('num')
-  print(num['Why'])
-  print('err')
-  print(err['Why'])
-  print('max err = {}'.format(np.max(np.fabs(err['Why']))))
-
-  for p in numerical['Wxh']:
-    err['Wxh'].flat[p]=np.fabs(numerical['Wxh'][p] - analytical['Wxh'].flat[p])
-    num['Wxh'].flat[p]=numerical['Wxh'][p]
-    #print(p - analytical.flatten())
-
-  print('num')
-  print(num['Wxh'])
-  print('err')
-  print(err['Wxh'])
-  print('max err = {}'.format(np.max(np.fabs(err['Wxh']))))
+  checkgrads(xs, encoder, ts, analytical)
